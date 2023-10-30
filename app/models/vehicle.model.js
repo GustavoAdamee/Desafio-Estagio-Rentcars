f028340a2c3e0b22aa63b807772ec746490f32b2
Adame <gusadomingues@gmail.com>
2023-10-30 14:31:36 -0300

    feat: vehicle structure created

--- app/models/vehicle.model.js
+++ app/models/vehicle.model.js
@@ -2,17 +2,36 @@
 
 module.exports = (connection, Sequelize) => {
     const Vehicle = connection.define("vehicle",{
-        id: {},
-        locadora:{},
-        modelo:{},
-        marca:{},
-        ano:{},
-        motor:{},
-        portas:{},
-        cambio:{},
-        ar_condicionado:{},
-        updated_at:{},
-        created_at:{}
+        id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        locadora:{
+            type: Sequelize.STRING
+        },
+        modelo:{
+            type: Sequelize.STRING
+        },
+        marca:{
+            type: Sequelize.STRING
+        },
+        ano:{
+            type: Sequelize.INTEGER
+        },
+        motor:{
+            type: Sequelize.STRING
+        },
+        portas:{
+            type: Sequelize.INTEGER
+        },
+        cambio:{
+            type: Sequelize.STRING
+        },
+        ar_condicionado:{
+            type: Sequelize.BOOLEAN
+        }
     });
 
     return Vehicle;
