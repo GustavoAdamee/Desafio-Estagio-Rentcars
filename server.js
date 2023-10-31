60e3c4648dc8b730b5885e4c7a3fbe0d36875ab8
Adame <gusadomingues@gmail.com>
2023-10-31 11:13:53 -0300

    fix: server init

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-require('dotenv').config();
+require('dotenv').config()
 
-app.use(express.json());
+app.use(express.json())
+
+const db = require('./app/models')
+
+db.connection.sync({ force: true })
+    .then(() =>{
+        console.log('Drop and Resync Database with { force: true }')
+    })
+    .catch((err) => {
+        console.log("Failed to sync db: " + err)
+    })
 
 app.get('/', (req, res) => {
     res.send('Welcome to RENTCARS API');
-});
-
-const db = require('./app/models');
-
-db.connection.sync({ force: true })
-.then(() =>{
-    console.log('Drop and Resync Database with { force: true }');
 })
-.catch((err) => {
-    console.log("Failed to sync db: " + err);
-});
 
+// Routes
+require('./app/routes/vehicle.routes')(app)
 
 const PORT = process.env.SERVER_PORT
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
